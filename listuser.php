a9340dd9c1ed3647c777f25b629c77e73b205ce7
kurtRuzzelSapo <wkurtruzzelsapo@gmail.com>
2024-02-26 02:26:32 +0800

    Added a delete feature
    to delete the account of the user incase of creating a dump recipes

--- listuser.php
+++ listuser.php
@@ -1,4 +1,8 @@
+<?php
+session_start();
+require_once(__DIR__ . '/pagesPHP/connection.php');
 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,12 +11,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="../assets/Logo.png" type="image/x-icon" />
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
 
     <link rel="stylesheet" href="./style/homepage.css" />
     <script src="../js/app.js"></script>
     <script src="../js/hamburger.js"></script>
-   <script src="get_users.js"></script>
+    <script src="get_users.js"></script>
     <title>Aroma Kitchen</title>
 </head>
 
@@ -27,7 +31,7 @@
         <div class="navbar-links">
             <ul>
                 <li>
-                    <a href="listrecipe.php">Recipes</a>
+                    <a href="homepage_admin.php">Recipes</a>
                 </li>
                 <li>
                     <a id="in-page" href="listuser.php">Users</a>
@@ -56,7 +60,76 @@
         </a>
     </section>
     <section class="saved-recipe-data">
-     
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mt-4">
+
+                    <?php if (isset($_SESSION['message'])) : ?>
+                        <h5 class="alert alert-success">
+                            <?= $_SESSION['message']; ?>
+                        </h5>
+                    <?php
+                        unset($_SESSION['message']);
+                    endif; ?>
+
+
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+
+                                        <th>ID</th>
+                                        <th>Email</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    try {
+                                        $query = "SELECT * FROM users";
+                                        $stmt = $pdo->prepare($query);
+                                        $stmt->execute();
+                                        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                                        if ($result) {
+                                            foreach ($result as $row) {
+                                    ?>
+                                                <tr>
+                                                    <td><?= $row['id'] ?></td>
+                                                    <td><?= $row['email'] ?></td>
+
+                                                    <td>
+                                                        <form action="code.php" method="POST">
+                                                            <button type="submit" value="<?= $row['id']; ?>" name="delete_user" class="btn btn-danger">Delete</button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            <?php
+                                            }
+                                        } else {
+                                            ?>
+                                            <tr>
+                                                <td colspan="4">No records found</td>
+                                            </tr>
+                                    <?php
+                                        };
+                                    } catch (PDOException $e) {
+                                        echo $e->getMessage();
+                                    }                                   ?>
+                                    <tr>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
     </section>
     <footer class="footer-sec">
         <img class="logo-footer" src="../assets/Logo.png" alt="" />
